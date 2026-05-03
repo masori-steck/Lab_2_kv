2fa82f54ae2a34e3ce5c02273beaf24f0d556427
Destructive212 <vladxiaomivlad@gmail.com>
2026-05-03 14:03:09 +0300

    Updated App.jsx

--- lab_2_react/src/App.jsx
+++ lab_2_react/src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
-import Reviews from './components/reviews';        
+import Reviews from './components/Reviews';        
 import ContactForm from './components/ContactForm'; 
 
 function App() {
